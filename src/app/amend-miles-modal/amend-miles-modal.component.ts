a3b4880ca1a8dcd78da3fa29005f2706b9a3b679
kishankumar03 <kishankumarpagadala@gmail.com>
2021-09-02 18:18:32 -0500

    2 files modified

--- src/app/amend-miles-modal/amend-miles-modal.component.ts
+++ src/app/amend-miles-modal/amend-miles-modal.component.ts
@@ -7,14 +7,23 @@ import {
   TemplateRef
 } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FollowUpModalComponent } from '../follow-up-modal/follow-up-modal.component';
 import { Plain } from '../model/plain.model';
+import { popUpModalService } from '../popup-modal-service';
 
 @Component({
   selector: 'amend-miles',
   templateUrl: './amend-miles-modal.component.html'
 })
 export class AmendMilesModalComponent implements OnInit {
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    public popupMpdalSerive: popUpModalService
+  ) {}
 
   ngOnInit() {}
+
+  openAnotherModal() {
+    this.popupMpdalSerive.openModalWithComponent(FollowUpModalComponent);
+  }
 }
